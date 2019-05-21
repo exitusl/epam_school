55c1d5eba7e8a1bf17c2a89232a49384be1badcd
Dudkina_OV <dudkina-olia@mail.ru>
2019-05-21 13:26:00 +0300

    authorization is work

--- src/test/java/testclass.java
+++ src/test/java/testclass.java
@@ -16,7 +16,7 @@ public class testclass {
     private String login="test_olga";
     private String password="123456";
 
-    @BeforeTest
+    @BeforeMethod
     public void createDriver() {
         System.setProperty("webdriver.chrome.driver", "src/main/tools/chromedriver.exe");
         driver = new ChromeDriver();
@@ -24,8 +24,7 @@ public class testclass {
  //       driver.navigate().to("http://localhost/wordpress/wp-login.php");
         driver.navigate().to("http://v999140x.beget.tech/wp-login.php");
         driver.manage().timeouts().pageLoadTimeout(60, TimeUnit.SECONDS);
-
-
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
     }
 
     @Test
@@ -33,12 +32,17 @@ public class testclass {
     public void Authorization() {
 
         WebElement loginUser = driver.findElement(By.xpath("//input[@id='user_login']"));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
   //      loginUser.sendKeys("user1");
         loginUser.sendKeys(login);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement loginPassword = driver.findElement(By.xpath("//input[@id='user_pass']"));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
   //      loginPassword.sendKeys("user1@gmail.com");
         loginPassword.sendKeys(password);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         WebElement btnEnter = driver.findElement(By.xpath("//p[@class='submit']/input[@type='submit']"));
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         btnEnter.click();
         WebElement displayName = driver.findElement(By.xpath("//a[contains(text(),'Привет')]/span"));
   /*      Actions actions = new Actions(driver);
@@ -51,16 +55,23 @@ public class testclass {
 
     }
 
-    @Test
+    @Test ()
    public void addArticle() {
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        Authorization();
         WebElement add = driver.findElement(By.xpath("//div[@aria-label='Верхняя панель']//span[contains (text(), 'Добавить')]"));
-     //   Actions actions = new Actions(driver);
-     //   actions.moveToElement(add).build().perform();
-     //   WebElement addNote = driver.findElement(By.xpath("//li[@class='menupop']//a[contains(text(),'Запись')]"));
-     //   addNote.click();
-        add.click();
+        Actions actions = new Actions(driver);
+        actions.moveToElement(add).build().perform();
+        WebElement addNote = driver.findElement(By.xpath("//li[@class='menupop']//a[contains(text(),'Запись')]"));
+        addNote.click();
+     //   add.click();
         WebElement btnPublish =driver.findElement(By.xpath("//div[@class='edit-post-header__settings']//button[contains(text(),'Опубликовать')]"));
         btnPublish.isDisplayed();
+        if (driver.findElement(By.xpath("//div[@class='components-popover__content']")).isDisplayed())
+        {
+            driver.findElement(By.xpath("//div[@class='components-popover__content']")).click();
+        }
+
         WebElement textHeadline =driver.findElement(By.xpath("//div[@class='editor-post-title']//textarea"));
         textHeadline.click();
         textHeadline.sendKeys("New Headline");
@@ -78,9 +89,10 @@ public class testclass {
     }
 
 
-
-    @AfterTest
+    @AfterMethod
     public void quitDriver() {
-        driver.quit();
+        if (driver!=null) {
+            driver.quit();
+        }
     }
 }
